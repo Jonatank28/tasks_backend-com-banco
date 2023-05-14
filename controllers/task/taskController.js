6725aa82efa10082c08034445737f0fb70a742a9
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-05-13 21:06:45 -0300

    botão status add

--- controllers/task/taskController.js
+++ controllers/task/taskController.js
@@ -29,8 +29,10 @@ class TaskController {
         const data = req.body;
         const { id } = req.params;
 
-        const sqlUpdateTask = `UPDATE task SET title = ?, description = ?, tagID = ?, priorityID = ? WHERE taskID = ?`;
-        const [row] = await db.promise().query(sqlUpdateTask, [data.title, data.description, data.tagID, data.priorityID, id]);
+        console.log(data);
+
+        const sqlUpdateTask = `UPDATE task SET title = ?, description = ?, tagID = ?, priorityID = ?, finished = ? WHERE taskID = ?`;
+        const [row] = await db.promise().query(sqlUpdateTask, [data.title, data.description, data.tagID, data.priorityID, data.finished, id]);
         return res.status(200).json(row);
     }
 
